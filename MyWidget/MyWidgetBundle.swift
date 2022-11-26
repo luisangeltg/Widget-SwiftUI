0f205adea9137558ee1fd6ff9bd7971d00171329
Luis Angel Torres G <t.g.luisangel@hotmail.com>
2022-11-26 05:28:14 -0600

    first commit

---
+++ MyWidget/MyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MyWidgetBundle.swift
+//  MyWidget
+//
+//  Created by Luis Angel Torres G on 25/11/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyWidget()
+    }
+}
