7ffb92f044fecea17179bd00e25a7cd60bfb1159
Luis Angel Torres G <t.g.luisangel@hotmail.com>
2022-11-25 21:10:08 -0600

    Initial Commit

---
+++ Widget/WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetApp.swift
+//  Widget
+//
+//  Created by Luis Angel Torres G on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
